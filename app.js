d97a01f4da4bd3d176de0e934f1c2cc856313225
Rafael <69431917+rafaelleitedasilva@users.noreply.github.com>
2022-11-08 15:50:01 -0300

    app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+let express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.listen(port,() => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
